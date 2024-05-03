b9d6278a5870c25b6537781715739c4ea3732812
Ng8522 <105772971+Ng8522@users.noreply.github.com>
2024-05-03 19:43:00 +0800

    Add files via upload

--- spotify_music_app.py
+++ spotify_music_app.py
@@ -23,10 +23,7 @@ def collaborative_filtering(track_title, music_data):
     track_index = music_data_cleaned[music_data_cleaned['Track'] == track_title].index[0]
     similar_tracks = list(enumerate(similarity_matrix[track_index]))
     sorted_similar_tracks = sorted(similar_tracks, key=lambda x: x[1], reverse=True)
-    top_similar_tracks = sorted_similar_tracks[1:11]
-    top_indices = [index for index, _ in top_similar_tracks]
-    top_track_titles = music_data_cleaned.iloc[top_indices]['Track'].tolist()
-    return top_track_titles
+    return sorted_similar_tracks
 
 # Function to handle the recommendation process
 def recommend(user_input, track_titles, music_data):
@@ -45,12 +42,14 @@ def recommend(user_input, track_titles, music_data):
         if collab_filtering_result:
             st.success(f"🎶 Top 10 tracks similar to '{closest_match}' based on Collaborative Filtering:")
             table_data = []
-            for i, track_title in enumerate(collab_filtering_result[:10], start=1):
-                track_row = music_data[music_data['Track'] == track_title].iloc[0]
-                artist = track_row['Artist']
-                spotify_url = track_row['Url_spotify']
+            for idx, (track_idx, similarity_score) in enumerate(collab_filtering_result, start=1):
+                if idx > 10:
+                    break
+                similar_track = music_data.iloc[track_idx]
+                artist = similar_track['Artist']
+                spotify_url = similar_track['Url_spotify']
                 spotify_link = f"<a href='{spotify_url}' target='_blank' style='color: #1DB954; text-decoration: none;'>Listen on Spotify</a>"
-                table_data.append((i, track_title, artist, spotify_link))
+                table_data.append((idx, similar_track['Track'], artist, similarity_score, similar_track['Likes'], similar_track['Views'], spotify_link))
             
             st.markdown("""
             <style>
@@ -95,7 +94,7 @@ def recommend(user_input, track_titles, music_data):
             </style>
             """, unsafe_allow_html=True)
 
-            df = pd.DataFrame(table_data, columns=["No.", "Song's name", "Artist", "Spotify"])
+            df = pd.DataFrame(table_data, columns=["No.", "Song's name", "Artist", "Similarity Score", "Likes", "Views", "Spotify"])
             st.write(df.to_html(escape=False, classes='styled-table'), unsafe_allow_html=True)
 
 # Main function
@@ -115,3 +114,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
